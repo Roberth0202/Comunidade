697fc31e7a600b2455e1dd752981f6c62724ea4e
Roberth <roberthverdan20@gmail.com>
2025-10-24 21:09:09 -0300

    teste 2

--- comuna/settings.py
+++ comuna/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ['*', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
